72fcd5431f00e3083939f0e1dce9600bd66f48fa
RayaneDjr <106350839+RayaneDjr@users.noreply.github.com>
2023-03-03 18:23:54 +0100

    added background

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import Header from "./layout/Header";
 function App() {
   return (
     <div className='App'>
+      <img
+        className='bg'
+        src='https://www.sushitimes.fr/wp-content/uploads/2020/04/italian-food-background-1.jpg'
+        alt='bg'
+      />
       <Header />
       <Gallery />
       <Carte />
